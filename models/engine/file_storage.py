7619fb9d503ff009622b9f23d0de5337d86becbe
Sir-Blow <emmanuelakej@gmail.com>
2023-05-26 11:54:25 -0700

    add back classlist

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,6 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-"""
 class_list = {
               "BaseModel": BaseModel,
               "User": User,
@@ -27,7 +26,6 @@ class_list = {
               "Place": Place,
               "Review": Review
              }
-"""
 
 
 class FileStorage:
